f8666ad26f48f67e1de8aa11c9a46de66cb50930
kosirobwada <kosiro131219@gmail.com>
2023-12-26 17:17:27 +0900

    add DOM

--- DOM/EventBasics/app.js
+++ DOM/EventBasics/app.js
@@ -1,37 +1,79 @@
+// const btn = document.querySelector('#v2');
+
+// btn.onclick = function () {
+//     console.log('クリックした！');
+//     console.log('ほげほげ');
+// }
+
+// function scream() {
+//     console.log('ああああああああ！！');
+//     console.log('いやーーーーー');
+// }
+
+// btn.onmouseenter = scream;
+
+// document.querySelector('h1').onclick = () => {
+//     alert('h1をクリック');
+// }
+
+// const btn3 = document.querySelector('#v3');
+// btn3.addEventListener('click', function () {
+//     alert('クリック！！！');
+// });
+
+// function hoge() {
+//     console.log('hoge');
+// }
+
+// function moge() {
+//     console.log('moge');
+// }
+
+// const hogemogeButton = document.querySelector('#hogemoge');
+// // hogemogeButton.onclick = hoge;
+// // hogemogeButton.onclick = moge;
+
+// hogemogeButton.addEventListener('click', hoge);
+// hogemogeButton.addEventListener('click', moge);
+
 const btn = document.querySelector('#v2');
 
+// console.dir(btn)
+
 btn.onclick = function () {
-    console.log('クリックした！');
-    console.log('ほげほげ');
+    console.log('クリックした');
+    console.log('hogehoge')
 }
 
-function scream() {
-    console.log('ああああああああ！！');
-    console.log('いやーーーーー');
+function screem() {
+    console.log('AAAAAAAAAAAAAAAAA')
 }
 
-btn.onmouseenter = scream;
+btn.onmouseenter = screem;
 
-document.querySelector('h1').onclick = () => {
+document.querySelector('h1').onclick = function () {
     alert('h1をクリック');
 }
 
 const btn3 = document.querySelector('#v3');
-btn3.addEventListener('click', function () {
-    alert('クリック！！！');
-});
+btn3.addEventListener('mouseup', function () {
+    alert('クリック')
+})
 
 function hoge() {
-    console.log('hoge');
+    console.log('hoge')
 }
 
 function moge() {
-    console.log('moge');
+    console.log('moge')
 }
 
-const hogemogeButton = document.querySelector('#hogemoge');
+const hogemogeButton = document.querySelector('#hogemoge')
+
 // hogemogeButton.onclick = hoge;
+
 // hogemogeButton.onclick = moge;
 
-hogemogeButton.addEventListener('click', hoge);
-hogemogeButton.addEventListener('click', moge);
+hogemogeButton.addEventListener('click', hoge, { once: true })
+
+hogemogeButton.addEventListener('click', moge, { once: true })
--- DOM/RandomColors/app.js
+++ DOM/RandomColors/app.js
@@ -1,16 +1,53 @@
-const button = document.querySelector('button');
-const h1 = document.querySelector('h1');
+// const button = document.querySelector('button');
+// const h1 = document.querySelector('h1');
+
+// button.addEventListener('click', () => {
+//     const newColor = makeRandColor();
+//     document.body.style.backgroundColor = newColor;
+//     h1.innerText = newColor;
+// });
+
+// const makeRandColor = () => {
+//     const r = Math.floor(Math.random() * 256);
+//     const g = Math.floor(Math.random() * 256);
+//     const b = Math.floor(Math.random() * 256);
+
+//     return `rgb(${r}, ${g}, ${b})`;
+// }
+
+const button = document.querySelector('button')
+
+const h1 = document.querySelector('h1')
 
 button.addEventListener('click', () => {
-    const newColor = makeRandColor();
-    document.body.style.backgroundColor = newColor;
-    h1.innerText = newColor;
-});
+    // console.log('AAA')
+    // document.body.style.backgroundColor = 'olive'
+
+    // const newColor = makeRandColor()
+
+    const r = Math.floor(Math.random() * 256) + 1;
+    const g = Math.floor(Math.random() * 256) + 1;
+    const b = Math.floor(Math.random() * 256) + 1;
+
+    document.body.style.backgroundColor = `rgb(${r},${g},${b})`
+
+    const sum = r + g + b;
+
+    if (sum > 500) {
+        h1.style.color = 'white'
+    }
+    else {
+        h1.style.color = 'black'
+    }
+
+    h1.innerText = newColor
+
+})
 
 const makeRandColor = () => {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
+    const r = Math.floor(Math.random() * 256) + 1;
+    const g = Math.floor(Math.random() * 256) + 1;
+    const b = Math.floor(Math.random() * 256) + 1;
 
-    return `rgb(${r}, ${g}, ${b})`;
+    return `rgb(${r},${g},${b})`
 }
