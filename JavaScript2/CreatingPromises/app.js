d618e6d2c036883a448bd4dd6fc31e05fae9c2d8
kosirobwada <kosiro131219@gmail.com>
2023-12-27 23:17:11 +0900

    add Promise

--- JavaScript2/CreatingPromises/app.js
+++ JavaScript2/CreatingPromises/app.js
@@ -46,19 +46,45 @@ const fakeRequest = (url) => {
 //     })
 // });
 
+// const delayedColorChange = (color, delay) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             document.body.style.backgroundColor = color;
+//             resolve();
+//         }, delay);
+//     });
+// }
+
+// delayedColorChange('red', 1000)
+//     .then(() => delayedColorChange('orange', 1000))
+//     .then(() => delayedColorChange('yellow', 1000))
+//     .then(() => delayedColorChange('green', 1000))
+//     .then(() => delayedColorChange('blue', 1000))
+//     .then(() => delayedColorChange('indigo', 1000))
+//     .then(() => delayedColorChange('violet', 1000))
+
 const delayedColorChange = (color, delay) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             document.body.style.backgroundColor = color;
-            resolve();
-        }, delay);
-    });
+            resolve()
+        }, delay)
+    })
 }
 
-delayedColorChange('red', 1000)
-    .then(() => delayedColorChange('orange', 1000))
-    .then(() => delayedColorChange('yellow', 1000))
-    .then(() => delayedColorChange('green', 1000))
-    .then(() => delayedColorChange('blue', 1000))
-    .then(() => delayedColorChange('indigo', 1000))
-    .then(() => delayedColorChange('violet', 1000))
+// delayedColorChange('red', 1000)
+// delayedColorChange('orange', 1000)
+
+delayedColorChange('red', 1000).then(() => {
+    return delayedColorChange('orange', 1000)
+}).then(() => {
+    return delayedColorChange('yellow', 1000)
+}).then(() => {
+    return delayedColorChange('green', 1000)
+}).then(() => {
+    return delayedColorChange('blue', 1000)
+}).then(() => {
+    return delayedColorChange('indigo', 1000)
+}).catch(() => {
+
+})
--- JavaScript2/PromisesIntro/app.js
+++ JavaScript2/PromisesIntro/app.js
@@ -1,14 +1,42 @@
-// コールバック版
-const fakeRequestCallback = (url, success, failure) => {
-    const delay = Math.floor(Math.random() * 4500) + 500;
-    setTimeout(() => {
-        if (delay > 4000) {
-            failure('コネクションタイムアウト')
-        } else {
-            success(`ダミーデータ(${url})`)
-        }
-    }, delay)
-}
+// // コールバック版
+// const fakeRequestCallback = (url, success, failure) => {
+//     const delay = Math.floor(Math.random() * 4500) + 500;
+//     setTimeout(() => {
+//         if (delay > 4000) {
+//             failure('コネクションタイムアウト')
+//         } else {
+//             success(`ダミーデータ(${url})`)
+//         }
+//     }, delay)
+// }
+
+// fakeRequestCallback('books.com/page1', function (response) {
+//     console.log('成功1')
+//     console.log(response)
+//     fakeRequestCallback('books.com/page2', function (response) {
+//         console.log('成功2')
+//         console.log(response)
+//         fakeRequestCallback('books3.com/page3', function (response) {
+//             console.log('成功3')
+//             console.log(response)
+//         }, function (err) {
+//             console.log(err)
+//         })
+//     }, function (err) {
+//         console.log(err)
+//     })
+// }, function (err) {
+//     console.log(err)
+// })
+
+// fakeRequestCallback('books.com', function (response) {
+//     console.log('成功');
+//     console.log(response)
+// }, function (err) {
+//     console.log('エラー')
+//     console.log(err)
+// })
+
 // Promise版
 const fakeRequestPromise = (url) => {
     return new Promise((resolve, reject) => {
@@ -23,6 +51,46 @@ const fakeRequestPromise = (url) => {
     })
 }
 
+// const request = fakeRequestPromise('yelp.com/api/coffee')
+
+// request.then(() => {
+//     console.log('成功')
+// }).catch(() => {
+//     console.log('失敗')
+// })
+
+// request.then(() => {
+//     console.log('成功')
+// }).catch(() => {
+//     console.log('失敗')
+// })
+
+// fakeRequestPromise('yelp.com/api/coffee/page1').then(() => {
+//     console.log('成功')
+//     fakeRequestPromise('yelp.com/api/coffee/page2').then(() => {
+//         console.log('成功')
+//     }).catch(() => {
+//         console.log('失敗')
+//     }).catch(() => {
+//         console.log('失敗')
+//     })
+// })
+
+// fakeRequestPromise('yelp.com/api/coffee/page1').then((data) => {
+//     console.log('成功')
+//     console.log(data)
+//     return fakeRequestPromise('yelp.com/api/coffee/page2')
+// }).then((data) => {
+//     console.log('成功')
+//     console.log(data)
+//     return fakeRequestPromise('yelp.com/api/coffee/page3')
+// }).then((data) => {
+//     console.log('成功')
+//     console.log(data)
+// }).catch((err) => {
+//     console.log('失敗')
+//     console.log(err)
+// })
 
 // fakeRequestCallback('books.com/page1', function (response) {
 //     console.log('成功！！');
@@ -65,22 +133,46 @@ const fakeRequestPromise = (url) => {
 //         console.log('失敗1！！！！');
 //     });
 
-fakeRequestPromise('yelp.com/api/coffee/page1')
-    .then((data) => {
-        console.log('成功1！！！');
-        console.log(data);
-        return fakeRequestPromise('yelp.com/api/coffee/page2')
+// fakeRequestPromise('yelp.com/api/coffee/page1')
+//     .then((data) => {
+//         console.log('成功1！！！');
+//         console.log(data);
+//         return fakeRequestPromise('yelp.com/api/coffee/page2')
+//     })
+//     .then((data) => {
+//         console.log('成功2！！！');
+//         console.log(data);
+//         return fakeRequestPromise('yelp.com/api/coffee/page3')
+//     })
+//     .then((data) => {
+//         console.log('成功3！！！');
+//         console.log(data);
+//     })
+//     .catch((err) => {
+//         console.log('失敗！！！！');
+//         console.log(err);
+//     });
+
+// new Promise((resolve, reject) => {
+
+// })
+
+const fakeRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        const rand = Math.random()
+        setTimeout(() => {
+            if (rand < 0.7) {
+                resolve();
+            }
+            else {
+                reject();
+            }
+        }, 1000)
     })
-    .then((data) => {
-        console.log('成功2！！！');
-        console.log(data);
-        return fakeRequestPromise('yelp.com/api/coffee/page3')
-    })
-    .then((data) => {
-        console.log('成功3！！！');
-        console.log(data);
-    })
-    .catch((err) => {
-        console.log('失敗！！！！');
-        console.log(err);
-    });
+}
+
+fakeRequest('/hoge').then(() => {
+    console.log('成功')
+}).catch(() => {
+    console.log('エラー')
+})
